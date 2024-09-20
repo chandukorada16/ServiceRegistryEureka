26a0980132c06680631788e88c6082df4a5719df
chandukorada16 <chandukorada16@gmail.com>
2024-09-20 11:20:34 +0530

    inital commit by chandu

---
+++ src/test/java/com/acruent/microservices/eureka/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acruent.microservices.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
